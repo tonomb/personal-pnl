2fc5ff904aca6883b4bb5f36439f85be20e4b55a
Antonio MB <tono.mtzb@gmail.com>
2026-04-08 19:19:18 -0600

    feat: deletes example worker

--- apps/example-worker-echoback/README.md
+++
@@ -1,3 +0,0 @@
-# example-worker-echoback
-
-A simple example Worker. New Workers should use eslint.config.ts and tsconfig.json similar to the one here.
--- apps/example-worker-echoback/eslint.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig, getConfig } from '@repo/eslint-config'
-
-import type { Config } from '@repo/eslint-config'
-
-const config = getConfig(import.meta.url)
-
-export default defineConfig([config]) as Config
